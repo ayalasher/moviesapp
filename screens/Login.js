276e10769effe91f7e9fed80563b45bbe0f3c0de
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-11-11 18:34:08 +0300

    intial commit

---
+++ screens/Login.js
@@ -0,0 +1,11 @@
+import { Text, View , StyleSheet } from "react-native";
+
+export default function Login() {
+    return <View>
+        <Text>Login page</Text>
+    </View>
+}
+
+const styles=  StyleSheet.create({
+    
+})
---
+++ screens/Signup.js
@@ -0,0 +1,11 @@
+import { Text, View , StyleSheet } from "react-native";
+
+export default function Signup() {
+    return <View>
+        <Text>Sign up page</Text>
+    </View>
+}
+
+const styles=  StyleSheet.create({
+    
+})
